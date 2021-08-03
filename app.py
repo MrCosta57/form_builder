4f1f6cdbbca53264ec57fb9ff136e9cbe3f469a8
BuildTools <unconfigured@null.spigotmc.org>
2021-08-03 13:36:37 +0200

    Possibilità di modificare form aggiungendo domande

--- app.py
+++ app.py
@@ -315,18 +315,74 @@ def form_create():
     return render_template("form_create.html", user=current_user)
 
 
-@app.route("/form/<form_id>/add_question")
+@app.route("/form/<form_id>/add_question", methods=['GET', 'POST'])
 def form_add_question(form_id):
     form = db_session.query(Forms).filter(Forms.id == form_id).first()
-    return render_template("add_question.html", form=form)
+    if request.method == "POST":
+        req = request.form
+        if req.get("choose") == "si":
+            id_q = req.get("question_choose")
+            db_session.add(FormsQuestions(form_id=form_id, question_id=id_q))
+            db_session.commit()
+        else:
+            tag = req.get("tag_choose")
+            if tag == "nuovo":
+                new_tag = req.get("tag_aggiunto")
+                db_session.add(Tags(argument=new_tag))
+                db_session.commit()
+                tag = new_tag
+            tipo_domanda = req.get("tipo_domanda")
+            text_question = req.get("text_question")
+            print(tag)
+            id_t = (db_session.query(Tags.id).filter(Tags.argument == tag).first())[0]
+            if tipo_domanda == "open":
+                db_session.add(Questions(text=text_question))
+                db_session.commit()
+                id_q = (db_session.query(Questions.id).filter(Questions.text == text_question).first())[0]
+                db_session.add(OpenQuestions(id=id_q))
+                db_session.commit()
+                db_session.add(TagsQuestions(tag_id=id_t, question_id=id_q))
+                db_session.add(FormsQuestions(form_id=form_id, question_id=id_q))
+            elif tipo_domanda == "single":
+                db_session.add(Questions(text=text_question))
+                db_session.commit()
+                id_q = (db_session.query(Questions.id).filter(Questions.text == text_question).first())[0]
+                db_session.add(SingleQuestions(id=id_q))
+                db_session.commit()
+                db_session.add(TagsQuestions(tag_id=id_t, question_id=id_q))
+
+                number = req.get("number_answers")
+
+                for i in range(1, int(number)+1):
+                    cont = req.get(str(i))
+                    db_session.add(PossibleAnswersS(idPosAnswS=id_q, content=cont))
+                db_session.add(FormsQuestions(form_id=form_id, question_id=id_q))
+            elif tipo_domanda == "multiple_choice":
+                db_session.add(Questions(text=text_question))
+                db_session.commit()
+                id_q = (db_session.query(Questions.id).filter(Questions.text == text_question).first())[0]
+                db_session.add(MultipleChoiceQuestions(id=id_q))
+                db_session.commit()
+                db_session.add(TagsQuestions(tag_id=id_t, question_id=id_q))
+
+                number = req.get("number_answers")
+
+                for i in range(1, int(number)+1):
+                    cont = req.get(str(i))
+                    db_session.add(PossibleAnswersM(idPosAnswM=id_q, content=cont))
+                db_session.add(FormsQuestions(form_id=form_id, question_id=id_q))
+            db_session.commit()
+        return redirect(url_for('form_edit', form_id=form_id))
+    tags = db_session.query(Tags)
+    questions = db_session.query(Questions)
+    return render_template("question_add.html", form=form, tags=tags, questions=questions)
 
 
 @app.route("/form/<form_id>/edit")
 @auth_required()
 def form_edit(form_id):
-    # query con form id: ...
-    # Passo al template un oggetto di tipo Form, poi nel template farò le query in caso
-    return "Ciao da form_id edit"
+    form = db_session.query(Forms).filter(Forms.id == form_id).first()
+    return render_template("form_edit.html", user=current_user, questions=form.questions, form=form)
 
 
 @app.route("/form/<form_id>/viewform", methods=['GET', 'POST'])
